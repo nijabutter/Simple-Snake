a326a213834ca334202a4bba9ad7e19c883c584a
nijabutter <nijabutter@gmail.com>
2021-01-14 10:15:34 +0000

    Fix starting length. Added colors but commented

--- snake.c
+++ snake.c
@@ -13,27 +13,43 @@ int main()
 	int WIDTH = 20;
 	int HEIGHT = 20;
 	
-	const char SNAKE_CHAR = '#';
+	const char SNAKE_CHAR = '0';
 	const char GRID_CHAR = '.';
-	// const char FRUIT_CHAR = '𝜋';
 
 	srand(time(NULL));
 	initscr();
 	noecho();
 	cbreak();
-  	curs_set(0);
+  curs_set(0);
 	timeout(1);
+/*
+  if (has_colors() == FALSE) {
+    endwin();
+    printf("Your terminal does not support color\n");
+    exit(1);
+  }
 
-	int snake_X = 10;
-	int snake_Y= 10;
-	int tail_Length = 1;
+  use_default_colors();
+  start_color();
+  init_pair(1, COLOR_GREEN, -1);
+  init_pair(2, COLOR_RED, -1);
+  init_pair(3, COLOR_WHITE, -1);
+  init_pair(4, COLOR_BLACK, -1);
+*/
+  int snake_X = WIDTH/2;
+	int snake_Y= HEIGHT/2;
+	int tail_Length = 3;
 	int fruit_X = 15;
 	int fruit_Y = 15;
 	
 	int tailX[100], tailY[100];
-	tailX[0] = snake_X-1;
-	tailY[0] = snake_Y;
-	
+  
+  // set the position of the rest of the body to be behind the head
+  for (int t = 0; t < tail_Length; t++) { 
+    tailX[t] = snake_X-t-1;
+    tailY[t] = snake_Y;
+  }
+
 	enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 	enum eDirection direction;
 	direction = STOP;
@@ -43,9 +59,6 @@ int main()
 
 	while (running) {
 
-		/*
-		 * Input
-		 */
 		switch(getch()) {
 			case 'q':
 				running = false;
@@ -73,9 +86,6 @@ int main()
 				break;
 		}
 
-		/*
-		 * Logic
-		 */
 		if (snake_Y == fruit_Y && snake_X == fruit_X) {
 			// eaten the fruit
 			tail_Length++;
@@ -90,42 +100,43 @@ int main()
 			// off screen left or right
 			running = false;
 		}
-		int prevX = tailX[0];
-		int prevY = tailY[0];
-		int prev2X, prev2Y;
-		tailX[0] = snake_X;
-		tailY[0] = snake_Y;
-		for (int i = 1; i < tail_Length; i++) {
-			prev2X = tailX[i];
-			prev2Y = tailY[i];
-			tailX[i] = prevX;
-			tailY[i] = prevY;
-			prevX = prev2X;
-			prevY = prev2Y;
-		}
-		switch (direction) {
-			case UP:
-				snake_Y--;
-				break;
-			case DOWN:
-				snake_Y++;
-				break;
-			case LEFT:
-				snake_X--;
-				break;
-			case RIGHT:
-				snake_X++;
-				break;
-			default:
-				break;
-		}
-		
-		/*
-		 * Draw
-		 */
+		if (direction != STOP) {
+      int prevX = tailX[0];
+      int prevY = tailY[0];
+      int prev2X, prev2Y;
+      tailX[0] = snake_X;
+      tailY[0] = snake_Y;
+      for (int i = 1; i < tail_Length; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+      }
+      switch (direction) {
+        case UP:
+          snake_Y--;
+          break;
+        case DOWN:
+          snake_Y++;
+          break;
+        case LEFT:
+          snake_X--;
+          break;
+        case RIGHT:
+          snake_X++;
+          break;
+        default:
+          break;
+      }
+    }
+
 		erase();
+    // -1 to WIDTH/HEIGHT to include border
 		for (int y = -1; y <= HEIGHT; y++) {
 			for (int x = -1; x <= WIDTH; x++) {
+        //attron(COLOR_PAIR(3));
 					if (y == -1) {
 						if (x == -1) {
 							addch(ACS_ULCORNER);
@@ -155,31 +166,51 @@ int main()
 					}
 					else {
 						if (y == snake_Y && x == snake_X) {
-							addch(SNAKE_CHAR);
+              switch (direction) {
+                case UP:
+                  addch('^');
+                  break;
+                case DOWN:
+                  addch('v');
+                  break;
+                case LEFT:
+                  addch('<');
+                  break;
+                case RIGHT:
+                  addch('>');
+                  break;
+                default:
+                  addch(SNAKE_CHAR); // stopped
+                  break;
+              }
 						}
 						else if (y == fruit_Y && x == fruit_X) {
-							addch(FRUIT_CHAR);
+							//attron(COLOR_PAIR(2));
+              addch(FRUIT_CHAR);
 						}
 						else {
+              //attron(COLOR_PAIR(1));
 							bool printed = false;
 							for (int t = 0; t < tail_Length; t++) {
 								if (t != 0 && snake_Y == tailY[t] && snake_X == tailX[t]) {
 									running = false;
+                  break;
 								}
 								if (tailY[t] == y && tailX[t] == x) {
 									addch(SNAKE_CHAR);
 									printed = true;
-									break;
 								}
 							}
 							if (!printed) {
 								if (showGrid) {
+                  //attron(COLOR_PAIR(4));
 									addch(GRID_CHAR);
 								}
 								else {
 									addch(' ');
 								}
 							}
+              //attron(COLOR_PAIR(0));
 						}
 						addch(' '); // to make the horizontal match vertical
 					}
