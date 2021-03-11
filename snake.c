ba6ccaa67cfcac07ac56ea267bf5c13e11fc9700
nijabutter <nijabutter@gmail.com>
2021-03-11 03:39:29 +0000

    Refactor

--- snake.c
+++ snake.c
@@ -7,6 +7,12 @@
 
 #define FRUIT_CHAR ACS_PI
 
+void repeatChar(const int amount, const char character) {
+	for (int i = 0; i < amount; i++) {
+		addch(character);
+	}
+}
+
 int main()
 {
 	const int FRAME_DELAY = 72000;
@@ -26,12 +32,14 @@ int main()
 	timeout(1);
 
 	getmaxyx(stdscr, SCREEN_HEIGHT, SCREEN_WIDTH);
-	int indent_left = (SCREEN_WIDTH / 2) - WIDTH;
-	int indent_top = (SCREEN_HEIGHT - HEIGHT) / 2 -1;
-	/*
+
+    int indent_left, indent_top;
+	indent_left = (SCREEN_WIDTH / 2) - WIDTH;
+	indent_top = (SCREEN_HEIGHT - HEIGHT) / 2 -1;
+
 	if (has_colors() == FALSE) {
 		endwin();
-		printf("Your terminal does not support color\n");
+		printf("Sorry, your terminal does not support color.\n");
 		exit(1);
 	}
 
@@ -41,10 +49,11 @@ int main()
 	init_pair(2, COLOR_RED, -1);
 	init_pair(3, COLOR_WHITE, -1);
 	init_pair(4, COLOR_BLACK, -1);
-	*/
+
   	int snake_X = WIDTH/2;
 	int snake_Y= HEIGHT/2;
 	int tail_Length = 3;
+
 	int fruit_X = rand() % WIDTH;
 	int fruit_Y = rand() % HEIGHT;
 	
@@ -57,8 +66,7 @@ int main()
 	}
 
 	enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-	enum eDirection direction;
-	direction = STOP;
+	enum eDirection direction = STOP;
 	
 	bool running = true;
 	bool showGrid = false;
@@ -74,7 +82,7 @@ int main()
 					direction = UP;
 				break;
 			case 'a':
-				if (direction != RIGHT)
+				if (direction != RIGHT && direction != STOP)
 					direction = LEFT;
 				break;
 			case 's':
@@ -88,6 +96,11 @@ int main()
 			case ' ':
 				showGrid = !showGrid;
 				break;
+            case KEY_RESIZE: 
+                getmaxyx(stdscr, SCREEN_HEIGHT, SCREEN_WIDTH);
+                indent_left = (SCREEN_WIDTH / 2) - WIDTH;
+                indent_top = (SCREEN_HEIGHT - HEIGHT) / 2 -1;
+                break;
 			default:
 				break;
 		}
@@ -107,18 +120,19 @@ int main()
 			running = false;
 		}
 		if (direction != STOP) {
-		int prevX = tailX[0];
-		int prevY = tailY[0];
-		int prev2X, prev2Y;
-		tailX[0] = snake_X;
-		tailY[0] = snake_Y;
-		for (int i = 1; i < tail_Length; i++) {
-			prev2X = tailX[i];
-			prev2Y = tailY[i];
-			tailX[i] = prevX;
-			tailY[i] = prevY;
-			prevX = prev2X;
-			prevY = prev2Y;
+            int prevX = tailX[0];
+            int prevY = tailY[0];
+            int prev2X, prev2Y;
+            tailX[0] = snake_X;
+            tailY[0] = snake_Y;
+            for (int i = 1; i < tail_Length; i++) {
+                prev2X = tailX[i];
+                prev2Y = tailY[i];
+                tailX[i] = prevX;
+                tailY[i] = prevY;
+                prevX = prev2X;
+                prevY = prev2Y;
+			}
 		}
 		switch (direction) {
 			case UP:
@@ -136,103 +150,110 @@ int main()
 			default:
 			break;
 		}
-    }
+		
 		erase();
-    	// -1 to WIDTH/HEIGHT to include border
-		for (int p = 0; p < indent_top; p++) {
-			addch('\n');
-		}
-		for (int y = -1; y <= HEIGHT; y++) {
-			for (int p = 0; p < indent_left; p++) {
-					addch(' ');
-			}
-			for (int x = -1; x <= WIDTH; x++) {
-				//attron(COLOR_PAIR(3));
-				if (y == -1) {
-					if (x == -1) {
-						addch(ACS_ULCORNER);
-					}
-					else if (x == WIDTH) {
-						addch(ACS_URCORNER);
-					}
-					else {
-						addch(ACS_HLINE); // need two because of adding space
-						addch(ACS_HLINE);
-					}
-				}
-				else if (y == HEIGHT) {
-					if (x == -1) {
-						addch(ACS_LLCORNER);
-					}
-					else if (x == WIDTH) {
-						addch(ACS_LRCORNER);
-					}
-					else {
-						addch(ACS_HLINE);
-						addch(ACS_HLINE);
-					}
-				}
-				else if (x == -1 || x == WIDTH) {
-					addch(ACS_VLINE);
-				}
-				else {
-					if (y == snake_Y && x == snake_X) {
-						switch (direction) {
-							case UP:
-							addch('^');
-							break;
-							case DOWN:
-							addch('v');
-							break;
-							case LEFT:
-							addch('<');
-							break;
-							case RIGHT:
-							addch('>');
-							break;
-							default:
-							addch(SNAKE_CHAR); // stopped
-							break;
-						}
-					}
-					else if (y == fruit_Y && x == fruit_X) {
-						//attron(COLOR_PAIR(2));
-              			addch(FRUIT_CHAR);
-					}
-					else {
-						//attron(COLOR_PAIR(1));
-						bool printed = false;
-						for (int t = 0; t < tail_Length; t++) {
-							if (t != 0 && snake_Y == tailY[t] && snake_X == tailX[t]) {
-								running = false;
-								break;
-							}
-							if (tailY[t] == y && tailX[t] == x) {
-								addch(SNAKE_CHAR);
-								printed = true;
-							}
-						}
-						if (!printed) {
-							if (showGrid) {
-								//attron(COLOR_PAIR(4));
-								addch(GRID_CHAR);
-							}
-							else {
-								addch(' ');
-							}
-						}
-              			//attron(COLOR_PAIR(0));
-					}
-					addch(' '); // to make the horizontal match vertical
-				}
-			}
-			// finished drawing row
+        
+        // filler new lines to add the top indent
+		repeatChar(indent_top, '\n');
+		
+        // draw top border
+
+        // add left padding
+		repeatChar(indent_left, ' ');
+
+        addch(ACS_ULCORNER);
+
+        for (int i = 0; i < WIDTH; i++) {
+            addch(ACS_HLINE); // need two because of adding spaces after each char
+            addch(ACS_HLINE);
+        }
+		
+        addch(ACS_URCORNER);
+
+        addch('\n');
+
+		for (int y = 0; y < HEIGHT; y++) {
+
+            // add left padding
+			repeatChar(indent_left, ' ');
+
+            addch(ACS_VLINE); // left border
+
+            for (int x = 0; x < WIDTH; x++) {
+				
+                if (y == snake_Y && x == snake_X) {
+                    // the snake head
+                    switch (direction) {
+                        case UP:
+                        addch('^');
+                        break;
+                        case DOWN:
+                        addch('v');
+                        break;
+                        case LEFT:
+                        addch('<');
+                        break;
+                        case RIGHT:
+                        addch('>');
+                        break;
+                        default:
+                        addch('>'); // stopped
+                        break;
+                    }
+                }
+                else if (y == fruit_Y && x == fruit_X) {
+                    // its the fruit
+                    attron(COLOR_PAIR(2));
+                    addch(FRUIT_CHAR);
+                    attroff(COLOR_PAIR(2));
+                }
+                else {
+                    // either blank or part of the snake body
+
+                    bool printed = false; // to know if we printed a snake body or if it is empty
+                    for (int t = 0; t < tail_Length; t++) {
+                        // check each part of the snake
+                        if (snake_Y == tailY[t] && snake_X == tailX[t]) {
+                            // snake body has hit head - game over
+                            running = false;
+                            break;
+                        }
+                        if (tailY[t] == y && tailX[t] == x) {
+                    		attron(COLOR_PAIR(1));
+                            addch(SNAKE_CHAR);
+                    		attroff(COLOR_PAIR(1));
+                            printed = true; // printed a body part - dont print a gap or dot
+                            break;
+                        }
+                    }
+                    if (!printed) { // gap/dot
+                        if (showGrid) {
+                            attron(COLOR_PAIR(4));
+                            addch(GRID_CHAR);
+                            attroff(COLOR_PAIR(4));
+                        }
+                        else {
+                            addch(' ');
+                        }
+                    }
+                }
+                addch(' '); // add space for padding, uniform in speed on x and y 
+            }
+			addch(ACS_VLINE);
 			addstr("\n");
 		}
-		// finished drawing
-		for (int p = 0; p < indent_left; p++) {
-			addch(' ');
+		repeatChar(indent_left, ' ');
+
+		addch(ACS_LLCORNER);
+		
+		for (int i = 0; i < WIDTH; i++) {
+			addch(ACS_HLINE);
+			addch(ACS_HLINE);
 		}
+
+		addch(ACS_LRCORNER);
+		addch('\n');
+		repeatChar(indent_left, ' ');
 		printw("Press q to quit");
 		refresh();
 		usleep(FRAME_DELAY);
